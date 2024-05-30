ad152d1d431d5834fe8b499037eb90ec0cea82fb
ameni <amenysakly@gmail.com>
2024-05-30 09:09:12 +0100

    Add _redirects file for client-side routing

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
+  app.use(express.static(join(__dirname, '..', 'client', 'dist', 'todo app')));
+  app.use((req, res, next) => {
+    res.sendFile(
+      join(__dirname, '..', 'client', 'dist', 'todo app', 'index.html'),
+    );
+  });
+
   await app.listen(3000);
 }
 bootstrap();
